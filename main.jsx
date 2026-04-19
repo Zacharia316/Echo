d8b608ffbcdaeb15430aba89306ec96c55001ad5
Zacharia316 <zappyblues234@gmail.com>
2026-04-19 22:22:30 +0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import EchoApp from './Echo'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <EchoApp />
+  </React.StrictMode>
+)
